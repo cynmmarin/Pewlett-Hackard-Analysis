3964011003bf8413c8cd5ed4fa818b8c9851fe75
Cynthia <cynthiammarin@gmail.com>
2021-04-22 03:14:51 -0700

    Additional Lists

--- schema.sql
+++ schema.sql
@@ -177,11 +177,94 @@ ORDER BY de.dept_no;
 
 SELECT * FROM dept_retirement_info;
 
+-- Create List: Employee Information
+SELECT emp_no,
+    first_name,
+last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+SELECT * FROM emp_info
 
+DROP TABLE emp_info;
 
+-- Make new temp table, add aliases and join the three tables
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
 
+SELECT * FROM emp_info
 
+-- Create List: Management
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
 
+SELECT * FROM manager_info;
 
+-- Create List: Department Retirees
+-- Add department number to current_emp table
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
 
+SELECT * FROM dept_info;
+
+-- Create table only for Sales Team
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+di.dept_name
+INTO sales_dept_info
+FROM retirement_info AS ri
+INNER JOIN dept_info AS di
+ON ri.emp_no = di.emp_no
+WHERE di.dept_name = ('Sales');
+
+SELECT * FROM sales_dept_info;
+
+-- Create table only for Sales and Development Team
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+di.dept_name
+INTO development_sales_dept_info
+FROM retirement_info AS ri
+INNER JOIN dept_info AS di
+ON ri.emp_no = di.emp_no
+WHERE di.dept_name IN ('Development','Sales');
+
+SELECT * FROM development_sales_dept_info;
